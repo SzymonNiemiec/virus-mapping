0d7b51382f32d5968e287e6e9dfaa992d8689be6
Cherasowy <cherras@outlook.com>
2020-03-30 15:42:00 +0200

    friends mapping

--- frontend/src/features/Friends/FriendItem.js
+++ frontend/src/features/Friends/FriendItem.js
@@ -1,12 +1,80 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const FriendsView = ({name,email,registered}) => {
+const FriendsView = ({ name, email, registered, ilnesses }) => {
     return (
         <div>
-<h1>{name}</h1>
-
+            <HistoryItem>
+                <Patient>
+                    <Name>{name}</Name>
+                    <Email href={`mailto:${email}`}>{email}</Email>
+                </Patient>
+                <Registered>
+                    <RegisterIndicator>Registered</RegisterIndicator>
+                    <p>{registered ? 'Yes' : 'No'}</p>
+                </Registered>
+                <Registered>
+                    <RegisterIndicator>Ilness</RegisterIndicator>
+                    <p>{ilnesses.length === 0 ? 'No' : 'Yes'}</p>
+                </Registered>
+                <LastMeet>
+                    
+                </LastMeet>
+            </HistoryItem>
         </div>
     )
 }
 
 export default FriendsView;
+
+const LastMeet = styled.div`
+
+`;
+
+
+const RegisterIndicator = styled.p`
+
+`
+
+
+const HistoryItem = styled.li`
+    padding:20px;
+    margin:10px;
+    display:grid;
+    align-items:center;
+    grid-template-columns: 3fr 2fr 3fr;
+    max-width:740px;
+    gap:10px;
+    border:1px solid #eee;
+    box-shadow: 0 2px 8px #f6f5f8;
+    position:relative;
+`;
+
+// primary: '#2a73b9',
+// secondary: '#3E64FF',
+// danger: '#C75454',
+// lightDanger: '#ffc4c4',
+// black: '#212121',
+// lightBlack: '#595959',
+// grey: '#f6f5f8',
+// darkGrey: "#C4C4C4",
+// background: '#FAFCFF',
+
+const Patient = styled.div``;
+
+const Registered = styled.div``;
+
+const Name = styled.p`
+    font-size:22px;
+    font-weight:600;
+`;
+
+const Email = styled.a`
+    font-size:14px;
+    color:#aaa;
+    text-decoration:none;
+    transition:300ms;
+    &:hover{
+        color:#2a73b9;
+    }
+`;
--- frontend/src/features/Friends/FriendsView.js
+++ frontend/src/features/Friends/FriendsView.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import FriendItem from './FriendItem'
+import styled from 'styled-components'
 
 const FriendsView = ({friends}) => {
     return (
-        <div>
-            {friends?.map( friend => <FriendItem name={friend?.name}/>)}
+        <FriendsWrapper>
+            <FriendsList>
+                {friends?.map( friend => <FriendItem name={friend?.name} registered={friend?.registered} email={friend?.email} ilnesses={friend?.illnesses}/>)}
+            </FriendsList>
+            
 
-        </div>
+        </FriendsWrapper>
     )
 }
 
 export default FriendsView;
+
+const FriendsList = styled.ul``;
+
+const FriendsWrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    width: 100%;
+    flex-direction: column
+
+`
