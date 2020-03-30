b979685da46b79b4020bb986f389e85465dc5ea5
SzymonNiemiec <saymek@o2.pl>
2020-03-30 14:35:23 +0200

    fixes

--- frontend/src/features/Sidebar/SidebarContainer.js
+++ frontend/src/features/Sidebar/SidebarContainer.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useEffect } from "react";
 import SidebarView from "./SidebarView";
 import { connect } from "react-redux";
 import { logoutUser } from "../../redux/modules/authentication";
+import { questionChange } from "../../redux/modules/survey";
+
 import axios from "axios";
 
 const SidebarContainer = ({ survey, logoutUser, user }) => {
@@ -12,7 +14,22 @@ const SidebarContainer = ({ survey, logoutUser, user }) => {
       "illnessDate": new Date()
     })
   };
-
+  useEffect(() => {
+    async function getLastSurvey() {
+      const response = await axios.get(
+        `http://localhost:5050/api/survey/user/5e80dee8e7466b1f0837f5e7/last`
+      );
+      const { data } = response;
+      if (new Date(data.date).toDateString() === new Date().toDateString()) {
+        if (data.cough) {
+          questionChange(5, 6);
+        } else {
+          questionChange(4, 5);
+        }
+      }
+    }
+    getLastSurvey()
+  }, []);
   return (
     <SidebarView
       logoutUser={logoutUser}
@@ -31,7 +48,9 @@ const mapStateToProps = state => ({
 
 const mapDispatchToProps = dispatch => {
   return {
-    logoutUser: () => dispatch(logoutUser())
+    logoutUser: () => dispatch(logoutUser()),
+    questionChange: (questionNumber, questionsCount) =>
+      dispatch(questionChange(questionNumber, questionsCount)),
   };
 };
 
